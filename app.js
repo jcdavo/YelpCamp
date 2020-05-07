f32a75d78f1d6f5f760ac153b4d0f52a3f9181e6
Davo <Davo@Davo-Mac.local>
2020-05-06 23:54:13 -0600

    Added Show.ejs Added description to our schema updated all places for description created logic to retreive id and use it for show.ejs

--- app.js
+++ app.js
@@ -27,34 +27,71 @@ app.use(
 // Schema for Mongo
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//   name: "Elbow Falls",
+//   image: "https://live.staticflickr.com/2646/3851438221_f98f6e435d_b.jpg",
+//   description: "Beautiful Elbow Falls, amazing star nights and great waterfall, no services"
+// }, (err, campground) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success! New Campground added!");
+//     console.log(campground);
+//   }
+// });
+
+// var campGrounds = [{
+//     name: "Elbow Falls",
+//     image: "https://live.staticflickr.com/2646/3851438221_f98f6e435d_b.jpg"
+//   },
+//   {
+//     name: "South Bragg Creek",
+//     image: "https://live.staticflickr.com/7390/9867372466_843f13b1c9_b.jpg"
+//   },
+//   {
+//     name: "Candle Lake",
+//     image: "https://live.staticflickr.com/8599/16710089445_7b8bcd92ed_b.jpg"
+//   },
+//   {
+//     name: "Lake wisp",
+//     image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"
+//   },
+// ];
+
 
 app.get("/", (req, res) => {
   res.render(`landing`)
 });
 
+// INDEX - Show all Campgrounds
 app.get("/campgrounds", (req, res) => {
   // Get all campgrounds from DB
   Campground.find({}, (err, campgrounds) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("campGrounds", {
+      res.render("index", {
         sites: campgrounds,
       });
     }
   })
 });
 
+// CREATE - Add new Campground to DB
 app.post("/campgrounds", (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
+  var description = req.body.description;
   var newCampground = {
     name: name,
-    image: image
+    image: image,
+    description: description
   };
   Campground.create(newCampground, (err, campground) => {
     if (err) {
@@ -67,9 +104,25 @@ app.post("/campgrounds", (req, res) => {
   });
 });
 
+// NEW - Display form for new Campground
 app.get("/campgrounds/new", (req, res) => {
   res.render("new.ejs")
 })
+
+// SHOW - Shows more info about one Campground
+app.get("/campground/:id", (req, res) => {
+  // find campground with provided id
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that campground
+      res.render("show", {
+        campground: foundCampground
+      });
+    }
+  });
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
   console.log("On-Line at 3000")
--- readme.md
+++ readme.md
@@ -28,3 +28,18 @@
 - Install and configure mongoose
 - Setup campground model
 - Use campground model inside of our routes!
+
+# Show Page
+
+- Review the RESTful routes we've seem so far
+- Add descriptors to our campground model
+- Show db.collection.drop()
+- add a show route/template
+
+Restful Routes
+Name ------ URL ------ verb ------ Desc.
+===========================================================
+INDEX ---- /dogs ------ GET ----- Display a list of all dog
+NEW ------ /dogs/new -- GET ----- Display form to make dog
+CREATE --- /dog ------- POST ---- Add new dog to DB
+SHOW ----- /dog/:id --- GET ----- Shows info about one dog
