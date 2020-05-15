6fc963a31777e3c75647a39fb0c788f333f96706
Davo <Davo@Davo-Mac.local>
2020-05-15 00:29:30 -0600

    Campgrounds.js Schema Update
    Update the Schema for campground adding a comment property and
    ref to the ID of the comments collection in the DB

--- models/campground.js
+++ models/campground.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
-  description: String
+  description: String,
+  comment: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment"
+  }]
 });
 module.exports = mongoose.model("Campground", campgroundSchema);
