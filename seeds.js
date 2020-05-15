b55cf09c055486f588be7acb233fb671f86a28dd
Davo <Davo@Davo-Mac.local>
2020-05-14 23:33:59 -0600

    Seed.js Updated and working
    Update code in the file to actually work.
    Arrow function was invalid before and modified to now work.
    DB.remove() is now deprecated, using DB.deleteMany() instead

--- seeds.js
+++ seeds.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose"),
   Campground = require("./models/campground");
 
-Campground.remove({}, err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Removed Campgrounds")
-  }
-});
+// .removed() is deprecated, .deleteMany() to be used
+const seedDB = () => {
+  Campground.deleteMany({}, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Removed Campgrounds")
+    }
+  });
+};
+
+module.exports = seedDB;
