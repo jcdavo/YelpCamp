48b8b5c2c43463de6f204ba801b0ed25d8cb10cc
Davo <Davo@Davo-Mac.local>
2020-05-14 23:05:00 -0600

    Seed DB File
    Created a SeedDB.js to beging creating the seed sequence
    Added remove campgrounds to seedDB.js
    Added seedDB in app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
-  Campground = require("./models/campground")
+  Campground = require("./models/campground"),
+  seedDB = require("./seeds");
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {
   useNewUrlParser: true,
