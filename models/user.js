b8b4a8e6290d7b2dfd2d1d04a557100db8ce7e82
Davo <Davo@Davo-Mac.local>
2020-05-18 15:42:05 -0600

    Create user.js
    Create user.js in models Dir.
    Create user schema using passpor-local-mongoose for auth.
    Export model.

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const passportLocalMongoose = require("passport-local-mongoose"),
+  mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
